7575f65dc14a6e08935bae9504e729faeb11a4ad
Raju Bibar -X (rbibar - HCL TECHNOLOGIES LIMITED at Cisco) <rbibar@cisco.com>
2017-02-13 17:32:07 +0530

    Added Comments

--- OwnFramework/src/com/java/cisco/myapp/semaphore/MainApp2.java
+++ OwnFramework/src/com/java/cisco/myapp/semaphore/MainApp2.java
@@ -2,6 +2,8 @@ package com.java.cisco.myapp.semaphore;
 
 public class MainApp2 {
  public static void main(String args[]){
+	//few Changes
+	//END of Changes
 	 System.out.println("Hey Man!!!");
  }
 }
